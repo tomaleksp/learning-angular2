5a2d384f1644f9a982b233525db61f7df7b2c7ff
tomaleksp <tomaleksp@poczta.fm>
2017-02-01 13:29:20 +0000

    seconds formatter

--- src/app/pomodoro-timer/pomodoro-timer.ts
+++ src/app/pomodoro-timer/pomodoro-timer.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pomodoro-timer',
     template: `
-        <h1> {{ minutes }} : {{ seconds }} </h1>
+        <h1> {{ minutes }} : {{ seconds | number: '2.0' }} </h1>
         <p>
             <button (click)="togglePause()"> {{ buttonLabel }} </button>
         </p>
@@ -31,7 +31,7 @@ export class PomodoroTimerComponent {
     tick() : void {
         if (!this.isPaused){
             this.buttonLabel = 'Pause';
-            
+
             if (--this.seconds < 0) {
             this.seconds = 59;
             if (--this.minutes < 0) {
